014a1e48adaf41476fb6e47c69d095ebd1bc79c1
RawIron <michael_nathe@hotmail.com>
2013-02-26 10:07:50 -0800

    use mockito

--- gameCommons/balanceSettings/DataItemLevel.java
+++ gameCommons/balanceSettings/DataItemLevel.java
@@ -4,4 +4,12 @@ public class DataItemLevel {
 	public int level;
 	public int xpNeeded;
 	public int rewarded;
+
+    public DataItemLevel() {}
+
+    public DataItemLevel(int level, int xpNeeded, int rewarded) {
+        this.level = level;
+        this.xpNeeded = xpNeeded;
+        this.rewarded = rewarded;
+    }
 }
