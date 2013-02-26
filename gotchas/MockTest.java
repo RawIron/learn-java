030ff9d2a3b6b7675fc4f57ca22712493e0cc7ba
RawIron <michael_nathe@hotmail.com>
2013-02-26 11:42:39 -0800

    mocktest ..

--- gotchas/MockTest.java
+++ gotchas/MockTest.java
@@ -6,11 +6,19 @@ import static org.mockito.Mockito.*;
 
 class Producer {
     public Product collect() {
-        return new Product();
+        return new Product("Unknown");
     }
 }
 
-class Product {}
+class Product {
+    String name;
+    public Product(String name) {
+        this.name = name;
+    }
+    public String is() {
+        return name;
+    }
+}
 
 
 public class MockTest extends TestCase {
@@ -20,11 +28,14 @@ public class MockTest extends TestCase {
 
     protected Producer topic() {
         Producer mocked = mock(Producer.class);
-        when(mocked.collect()).thenReturn(new Product());
+        when(mocked.collect()).thenReturn(new Product("Iron"));
         return mocked;
     }
 
     public final void test_should_have_product() {
         assertNotNull(topic().collect());
+    }
+    public final void test_should_collected_iron() {
+        assertEquals(topic().collect().is(), "Iron");
     }
 }
