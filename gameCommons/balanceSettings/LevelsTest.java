cd11ea501e6cd17abd766bc17d972b7bcaa56d2b
RawIron <michael_nathe@hotmail.com>
2013-02-27 02:42:12 -0800

    fix compile errors with mock

--- gameCommons/balanceSettings/LevelsTest.java
+++ gameCommons/balanceSettings/LevelsTest.java
@@ -8,16 +8,38 @@ import gameCommons.balanceSettings.DataLevel;
 import gameCommons.balanceSettings.DataItemLevel;
 
 
+class MockedLevels extends Levels {
+    public MockedLevels() {
+        super();
+    }
+    protected DataLevel create() {
+        DataLevel mock = mock(DataLevel.class);
+        verify(mock).read(12);
+        when(mock.read(12)).thenReturn(new DataItemLevel(12, 100, 10));
+        return mock;
+    }
+}
+
 public class LevelsTest extends TestCase {
+
     public LevelsTest() {
         super();
     }
 
+    protected Levels topic() {
+        return new MockedLevels();
+    }
+
     public final void test_should_need_100_xp() {
-        DataLevel mock = mock(DataLevel.class);
-        verify(mock).cached.get(12);
-        when(mock.cached.get(12)).thenReturn(new DataItemLevel(12, 100, 10));
-        DataItemLevel level = mock.cached.get(12);
-        assertEquals(level.level, 12);
+        assertEquals(topic().with(12).xpNeeded, 100);
+    }
+}
+
+
+class LevelsTestMain {
+    public static void main(String[] args) {
+        LevelsTest t = new LevelsTest();
+        Levels l = t.topic();
+        System.out.println(l.with(12).xpNeeded);
     }
 }
