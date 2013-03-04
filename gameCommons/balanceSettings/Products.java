5c6faedba2b726291e5325a569507dda456996e1
RawIron <michael_nathe@hotmail.com>
2013-03-04 06:15:24 -0800

    classes for export

---
+++ gameCommons/balanceSettings/Products.java
@@ -0,0 +1,27 @@
+package gameCommons.balanceSettings;
+
+import gameCommons.balanceSettings.DataProduct;
+import gameCommons.balanceSettings.DataItemProduct;
+
+
+/*
+Products product = new Products();
+product(5).xpNeeded
+product.5.xpNeeded 
+product.with(5).xpNeeded
+*/
+public class Products {
+    protected DataProduct settings = null;
+
+	public Products() {
+        this.settings = create();
+    }
+
+    public DataItemProduct with(int key) {
+        return settings.read(key);
+    }
+
+    protected DataProduct create() {
+        return new DataProduct();
+    }
+}
