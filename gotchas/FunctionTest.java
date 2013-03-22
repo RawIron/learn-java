bbd2845e1445317c5fda58a174c1bb2a98af3876
RawIron <michael_nathe@hotmail.com>
2013-03-21 21:54:15 -0700

    call by reference object

--- gotchas/FunctionTest.java
+++ gotchas/FunctionTest.java
@@ -5,19 +5,20 @@ import junit.framework.TestCase;
 
 
 class Function {
+    public int state = 2;
     public void giveInteger(Integer value) {
         value += 4;
     }
     public void giveArray(int[] value) {
         value[1] = 4;
     }
+    public void giveObject(Function value) {
+        value.state += 4;
+    }
 }
 
-public class FunctionTest extends TestCase {
 
-    public FunctionTest() {
-        super();
-    }
+public class FunctionTest extends TestCase {
 
     protected Function topic() {
         return new Function();
@@ -35,5 +36,11 @@ public class FunctionTest extends TestCase {
         int[] value = {1, 2};
         f.giveArray(value);
         assertEquals(value[1], 4);
+    }
+
+    public final void test_passObject() {
+        Function f = topic();
+        f.giveObject(f);
+        assertEquals(f.state, 6);
     }
 }
