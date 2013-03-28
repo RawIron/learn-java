168d0b32ce3eeda32a85c5477bef1bb60027cad5
RawIron <michael_nathe@hotmail.com>
2013-03-27 19:02:08 -0700

    tdd transaction

--- gameCommons/engine/TransactionTest.java
+++ gameCommons/engine/TransactionTest.java
@@ -11,4 +11,50 @@ public class TransactionTest extends TestCase {
         coins.add(100);
         assertEquals(coins.balance(), 100);
     }
+
+    public final void test_take100Coins() {
+        Accountable coins = new CoinsWallet();
+        coins.sub(100);
+        assertEquals(coins.balance(), -100);
+    }
+
+    public final void test_giveAndtake100Coins() {
+        Accountable coins = new CoinsWallet();
+        coins.add(100);
+        coins.sub(100);
+        assertEquals(coins.balance(), 0);
+    }
+
+    public final void test_transactionGiveBuildingTakeCoinsRollback() {
+        Accountable coins = new CoinsWallet();
+        Accountable building = new BuildingInventory();
+        Transaction t = new Transaction();
+        t.give(1, building).take(100, coins);
+        assertEquals(coins.balance(), 0);
+        assertEquals(building.balance(), 0);
+    }
+
+    public final void test_transactionGiveBuildingTakeCoinsCommit() {
+        Accountable coins = new CoinsWallet();
+        Accountable building = new BuildingInventory();
+        Transaction t = new Transaction();
+        t.give(1, building).take(100, coins).commit();
+        assertEquals(coins.balance(), -100);
+        assertEquals(building.balance(), 1);
+    }
+
+    public final void test_transactionGiveBuildingCommit() {
+        Accountable building = new BuildingInventory();
+        Transaction t = new Transaction();
+        t.give(1, building).commit();
+        assertEquals(building.balance(), 1);
+    }
+
+    public final void test_transactionTakeCoinsCommit() {
+        Accountable coins = new CoinsWallet();
+        Transaction t = new Transaction();
+        t.take(100, coins).commit();
+        assertEquals(coins.balance(), -100);
+    }
+
 }
