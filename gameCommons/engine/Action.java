db771c823ac073e795ebfc8c56d4900ff870e1f4
RawIron <michael_nathe@hotmail.com>
2013-03-31 19:16:14 -0700

    actions first version

--- gameCommons/engine/Action.java
+++ gameCommons/engine/Action.java
@@ -8,14 +8,23 @@ abstract class Action {
 }
 
 
+// mover.on(board).take(building.at(2,3)).putInto(storage)
 class StorageMover {
-    public void moveIt() {
-        Board board = new Board();
-        Boardable building = new Building();
-        Inventory storage = new Storage();
+    private int x;
+    private int y;
+    private Board board;
+    private Boardable item;
+    private Inventory storage;
 
-        board.remove(building).at(2,3);
-        storage.packAway(building);
+    public StorageMover() {}
+
+    public void on(Board board) {}
+    public void take(Boardable Item, int x, int y) {}
+    pubic void putInto(Inventory storage) {}
+
+    public void moveIt() {
+        board.remove(item.at(x,y));
+        storage.packAway(item);
     }
 }
 
