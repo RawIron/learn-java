3cfb15d940f135efa7d29cabd0ae86917a37cef9
RawIron <michael_nathe@hotmail.com>
2013-03-31 20:58:20 -0700

    tdd board place

--- gameCommons/engine/Action.java
+++ gameCommons/engine/Action.java
@@ -8,7 +8,7 @@ abstract class Action {
 }
 
 
-// mover.on(board).take(building.at(2,3)).putInto(storage)
+// mover.on(board).take(building.at(2,3)).stashItInto(storage)
 class StorageMover {
     private int x;
     private int y;
@@ -20,7 +20,7 @@ class StorageMover {
 
     public void on(Board board) {}
     public void take(Boardable Item, int x, int y) {}
-    pubic void putInto(Inventory storage) {}
+    pubic void stashItInto(Inventory storage) {}
 
     public void moveIt() {
         board.remove(item.at(x,y));
--- gameCommons/engine/BoardTest.java
+++ gameCommons/engine/BoardTest.java
@@ -12,4 +12,10 @@ public class BoardTest extends TestCase {
         board.place(building);
         assertEquals(board.on(0,0), Board.empty);
     }
+    public final void test_placeBuilding() {
+        Boardable building = new Building();
+        Board board = new Board();
+        board.place(building).at(0,0);
+        assertEquals(board.on(0,0), building);
+    }
 }
