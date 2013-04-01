9ff9cc40c9eabefd4ad13bb5d5258541a7b58a75
RawIron <michael_nathe@hotmail.com>
2013-04-01 09:33:53 -0700

    tdd board single method call

--- gameCommons/engine/sketches.java
+++ gameCommons/engine/sketches.java
@@ -10,10 +10,6 @@ class Sketches {
     on(player).rewards.listenOn(inventory)
     on(player).quests.listenOn([inventory,xp])
 
-    on(board).place(building).at(2,3)
-    on(board).move(building).from(2,3).to(4,5)
-    mover.run(move)
-
     make(transaction).from(building)
     transaction.give(xp)
     transaction.give(building).take(gold)
