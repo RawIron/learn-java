ea06c02bfc6b77e9c522d565fe6b3631068b6b63
RawIron <michael_nathe@hotmail.com>
2013-04-01 22:05:10 -0700

    tdd refactor board

--- gameCommons/engine/BoardTest.java
+++ gameCommons/engine/BoardTest.java
@@ -27,33 +27,4 @@ public class BoardTest extends TestCase {
         board.remove(building, new Point(1,1));
         assertEquals(board.on(1,1), Board.empty);
     }
-
-    public final void test_dottedPlaceIncomplete() {
-        Boardable building = new Building();
-        Board board = new Board();
-        board.place(building);
-        assertEquals(board.on(0,0), Board.empty);
-    }
-    public final void test_dottedPlaceBuilding() {
-        Boardable building = new Building();
-        Board board = new Board();
-        board.place(building).at(0,0);
-        assertEquals(board.on(0,0), building);
-    }
-    public final void test_dottedMoveBuilding() {
-        Boardable building = new Building();
-        Board board = new Board();
-        board.place(building).at(0,0);
-        board.move(building).from(0,0).to(1,1);
-        assertEquals(board.on(0,0), Board.empty);
-        assertEquals(board.on(1,1), building);
-    }
-    public final void test_dottedRemoveBuilding() {
-        Boardable building = new Building();
-        Board board = new Board();
-        board.place(building).at(1,1);
-        board.remove(building).at(1,1);
-        assertEquals(board.on(1,1), Board.empty);
-    }
-
 }
