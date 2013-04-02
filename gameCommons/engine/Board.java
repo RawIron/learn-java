607c8029a31839527b2323a18b921f9154fda30b
RawIron <michael_nathe@hotmail.com>
2013-04-01 22:05:10 -0700

    tdd refactor board

--- gameCommons/engine/Board.java
+++ gameCommons/engine/Board.java
@@ -1,12 +1,4 @@
 /*
-    board.place(building).at(2,3)
-    board.at(2,3).place(building)
-    board.move(building).from(2,3).to(4,5)
-
-    board.move(building, from, to)
-
-    move.is(place(building).at(2,3))
-    mover.make(move)
  */
 
 package gameCommons.engine;
@@ -35,118 +27,12 @@ class Move {
     }
 }
 
-
-abstract class BoardCommand {
-    protected LinkedList<String> words = new LinkedList<String>();
-    public abstract void init();
-
-    public BoardCommand() {
-        init();
-    }
-    public boolean isComplete() {
-        return (words.isEmpty());
-    }
-    public void found(String word) {
-        if (words.contains(word)) {
-            words.remove(word);
-        }
-    }
-}
-
-class BoardPlaceCommand extends BoardCommand {
-    @Override
-    public void init() {
-        words.add("place");
-        words.add("at");
-    }
-    public void run(Board board, Boardable piece, Point at) {
-        board.place(piece, at);
-    }
-}
-
-class BoardRemoveCommand extends BoardCommand {
-    @Override
-    public void init() {
-        words.add("remove");
-        words.add("at");
-    }
-    public void run(Board board, Boardable piece, Point at) {
-        board.remove(piece, at);
-    }
-}
-
-class BoardMoveCommand extends BoardCommand {
-    @Override
-    public void init() {
-        words.add("move");
-        words.add("from");
-        words.add("to");
-    }
-    public void run(Board board, Boardable piece, Point from, Point to) {
-        board.move(piece, from, to);
-    }
-}
-
-
 class Point {
     public int x;
     public int y;
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-}
-
-
-class DottedBoard {
-    int x = 0, y = 0;
-    Boardable piece = null;
-    BoardCommand command = null;
-
-    public Board remove(Boardable piece) {
-        this.piece = piece;
-        this.command = new BoardRemoveCommand();
-        return this;
-    }
-
-    public Board move(Boardable piece) {
-        this.piece = piece;
-        this.command = new BoardMoveCommand();
-        return this;
-    }
-
-    public Board place(Boardable piece) {
-        this.piece = piece;
-        this.command = new BoardPlaceCommand();
-        return this;
-    }
-
-    public Board at(int x, int y) {
-        this.x = x;
-        this.y = y;
-        if (command.isComplete()) {
-            command.run(this, piece, new Point(x,y));
-        } else {
-            return this;
-        }
-    }
-    public Board from(int x, int y) {
-        if (command.isComplete()) {
-            command.run(this, piece, new Point(x,y), new Point(this.x, this.y));
-        } else {
-            this.x = x;
-            this.y = y;
-            return this;
-        }
-    }
-    public Board to(int x, int y) {
-        if (command.isComplete()) {
-            command.run(this, piece, new Point(this.x, this.y), new Point(x,y));
-        } else {
-            this.x = x;
-            this.y = y;
-            return this;
-        }
     }
 }
 
