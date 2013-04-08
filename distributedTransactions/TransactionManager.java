b3804258f3f531dffb68e366dea6768eadbf69a9
RawIron <michael_nathe@hotmail.com>
2013-04-07 18:02:21 -0700

    tdd dtc state

--- distributedTransactions/TransactionManager.java
+++ distributedTransactions/TransactionManager.java
@@ -3,17 +3,26 @@ package distributedTransactions;
 
 public class TransactionManager {
 
+    public static final int Idle = -1;
     public static final int Open = 0;
-    public static final int Started = 0;
-    public static final int Prepared = 0;
-    public static final int Committed = 0;
+    public static final int Started = 1;
+    public static final int Prepared = 2;
+    public static final int Committed = 3;
 
     private int currentState;
 
-    public void open() {}
-    public void start() {}
-    public void prepare() {}
-    public void commit() {}
+    public void open() {
+        currentState = TransactionManager.Open;
+    }
+    public void start() {
+        currentState = TransactionManager.Started;
+    }
+    public void prepare() {
+        currentState = TransactionManager.Prepared;
+    }
+    public void commit() {
+        currentState = TransactionManager.Committed;
+    }
 
     public void resourceManagerIs(ResourceManager rm) {};
     public int state() { return currentState; }
