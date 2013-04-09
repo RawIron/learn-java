044bada825e2c92717b9a752662e6aaad50cadee
RawIron <manianology@gmail.com>
2013-04-09 07:49:52 -0700

    tdd RM

--- distributedTransactions/ResourceManager.java
+++ distributedTransactions/ResourceManager.java
@@ -2,7 +2,44 @@
 package distributedTransactions;
 
 
+class Resource {
+    public void write();
+}
+
+class LogManager {
+    public void write();
+}
+
+class LockManager {
+    public void lock();
+    public void release();
+}
+
+
 public class ResourceManager {
-    public void open() {}
-    public void close() {}
+    private LockManager lockm;
+    private LogManager logm;
+    private TransactionManager tm;
+    private Resource r;
+    
+    public ResourceManager(TransactionManager tm, LockManager lockm, LogManager logm) {
+        this.tm = tm;
+        this.lockm = lockm;
+        this.logm = logm;
+    }
+
+    public boolean isReady() {
+        tm.ready();
+    }
+    public void prepare() {
+        lockm.lock();
+        logm.write();
+        tm.readyToCommit();
+    }
+    public void commit() {
+        r.write();
+        logm.write();
+        lockm.release();
+        tm.commitSuccess();
+    }
 }
