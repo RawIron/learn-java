ae810392b0b144809a8ac9d347f76d6a4ee9daa1
RawIron <manianology@gmail.com>
2013-04-15 22:12:58 -0700

    tdd LockM

--- distributedTransactions/LockManager.java
+++ distributedTransactions/LockManager.java
@@ -2,28 +2,46 @@
 package distributedTransactions;
 
 import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+
+
+interface LockCallback {
+    void resourceIsAvailable();
+}
+
+class LockRequestQueue<T> extends ArrayBlockingQueue<T> {
+    public boolean isEmpty() {
+        return (this.size() < 1);
+    }
+}
 
 
 public class LockManager {
     Resource r = null;
     HashMap<Resource,Byte> locktable = new HashMap<Resource,Byte>();
-    Queue<Caller> waitQueue = new Queue<Caller>();
+    LockRequestQueue<LockCallback> waitQueue = new LockRequestQueue<LockCallback>();
 
     public LockManager(Resource resource) {
         this.r = resource;
     }
 
-    public boolean lock() {
+    public boolean lock(LockCallback callback) {
         if (locktable.containsKey(r)) {
-            waitQueue.put(caller);
+            waitQueue.put(callback);
             return false;
+        } else {
+            locktable.put(r,null);
+            return true;
         }
-        locktable.put(r,null);
-        return true;
     }
+
     public void release() {
+        if (!waitQueue.isEmpty()) {
+            LockCallback callback = waitQueue.take();
+            callback.resourceIsAvailable();
+            return;
+        }
         locktable.remove(r);
-        waitQueue.get(caller);
     }
 }
 
