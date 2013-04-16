9c8f9d0ecf09cd30b8de1af7b99ec0be78b3a3dc
RawIron <manianology@gmail.com>
2013-04-16 07:00:43 -0700

    tdd lockM lock

--- distributedTransactions/LockManagerTest.java
+++ distributedTransactions/LockManagerTest.java
@@ -2,17 +2,29 @@
 package distributedTransactions;
 
 import junit.framework.TestCase;
+import distributedTransactions.LockManager;
 
+
+class Callback implements LockCallback {
+    public void resourceIsAvailable() {
+    }
+}
 
 public class LockManagerTest extends TestCase {
-
-    public final void test_createLockM() {
+    private Callback callback;
+    private LockManager topic() {
+        Callback callback = new Callback();
         Resource resource = new Resource();
         LockManager lockM = new LockManager(resource);
-        assertNotNull(lockM);
+        return lockM;
+    }
+
+    public final void test_createLockM() {
+        assertNotNull(topic());
     }
 
     public final void test_acquireLock() {
+        assertTrue(topic().lock(callback));
     }
 
     public final void test_releaseExistingLock() {
