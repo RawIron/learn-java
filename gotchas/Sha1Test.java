4bea1139a362b914f0f4c619cf710e487c9961de
RawIron <manianology@gmail.com>
2014-06-27 09:14:13 -0700

    sha1

--- gotchas/Sha1Test.java
+++ gotchas/Sha1Test.java
@@ -4,25 +4,58 @@ import junit.framework.TestCase;
 
 
 import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class Sha1Test extends TestCase {
 
-    public final void test_string_bytes() {
-        String message = "a1";
-        byte [] bytes = null;
+  private String hexOfBytes(byte[] bytes) {
+    StringBuffer hexString = new StringBuffer();
+    for (int i = 0; i < bytes.length; i++)
+      hexString.append(Integer.toHexString(0xFF & bytes[i]));
+    return hexString.toString();
+  }
 
-        try {
-          bytes = message.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) { }
-
-        assertEquals(bytes.length, 2);
+  private String sha1(String salt, String message) {
+    MessageDigest digest = null;
+    try {
+      digest = MessageDigest.getInstance("SHA-1");
+    } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
     }
 
+    digest.update(message.getBytes());
+    byte[] messageDigest = digest.digest();
 
-    public final void test_sha1() {
-        String salt = "b2";
-        String message = "a1";
-        byte [] bytes = null;
-    }
+    return hexOfBytes(messageDigest);
+  }
+
+  private byte[] bytesOfString(String s) {
+    byte[] bytes = null;
+
+    try {
+      bytes = s.getBytes("UTF-8");
+    } catch (UnsupportedEncodingException e) { }
+
+    return bytes;
+  }
+
+
+  public final void test_string_bytes() {
+    String message = "a1";
+    byte[] bytes = null;
+
+    bytes = bytesOfString(message);
+    assertEquals(bytes.length, 2);
+    assertEquals((int) (bytes[0] & 0xff), 97);
+    assertEquals((int) (bytes[1] & 0xff), 49);
+  }
+
+  public final void test_sha1() {
+    String salt = "b2";
+    String message = "a1";
+    byte[] bytes = null;
+  }
+
 }
