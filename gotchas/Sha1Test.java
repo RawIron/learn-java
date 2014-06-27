eed1aa73ca8918a5aedf3060923cee11318414b2
RawIron <manianology@gmail.com>
2014-06-27 16:13:18 -0700

    sha1

--- gotchas/Sha1Test.java
+++ gotchas/Sha1Test.java
@@ -7,6 +7,11 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import java.security.SignatureException;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+
 
 public class Sha1Test extends TestCase {
 
@@ -23,6 +28,14 @@ public class Sha1Test extends TestCase {
         hexString.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
     }
     return hexString.toString();
+  }
+
+  private String hexOfBytes3(final byte[] bytes) {
+    // Convert raw bytes to Hex
+    //byte[] hexBytes = new Hex().encode(rawHmac);
+    //  Covert array of Hex bytes to a String
+    //return new String(hexBytes, "UTF-8");
+    return null;
   }
 
   private byte[] bytesOfString(final String s) {
@@ -44,9 +57,28 @@ public class Sha1Test extends TestCase {
     byte[] messageBytes = bytesOfString(message);
     byte[] saltBytes = bytesOfString(salt);
     sha1digest.update(messageBytes);
-    byte[] messageDigest = sha1digest.digest();
+    byte[] signature = sha1digest.digest();
 
-    return hexOfBytes2(messageDigest);
+    return hexOfBytes2(signature);
+  }
+
+  private String hmacSha1(final String key, final String value) {
+    try {
+        // Get an hmac_sha1 key from the raw key bytes
+        byte[] keyBytes = bytesOfString(key);
+        SecretKeySpec signingKey = new SecretKeySpec(keyBytes, "HmacSHA1");
+
+        // Get an hmac_sha1 Mac instance and initialize with the signing key
+        Mac mac = Mac.getInstance("HmacSHA1");
+        mac.init(signingKey);
+
+        // Compute the hmac on input data bytes
+        byte[] rawHmac = mac.doFinal(bytesOfString(value));
+
+        return hexOfBytes2(rawHmac);
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
   }
 
 
@@ -88,7 +120,7 @@ public class Sha1Test extends TestCase {
   public final void test_sha1_hmac() {
     String salt = "b2";
     String message = "a1";
-    String signature = sha1(salt, message);
+    String signature = hmacSha1(salt, message);
     assertEquals("240d6b81caf2ee8cb4161065095ac681fc0ce44a", signature);
   }
 
