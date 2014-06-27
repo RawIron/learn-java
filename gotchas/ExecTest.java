417f3c787929a703c4f4c6a056de5c803a5f291e
RawIron <manianology@gmail.com>
2014-06-27 07:58:12 -0700

    fix test

--- gotchas/ExecTest.java
+++ gotchas/ExecTest.java
@@ -6,7 +6,7 @@ import gotchas.Exec;
 
 public class ExecTest extends TestCase {
 
-    public final void test_runWEcho() {
+    public final void test_runEcho() {
         SystemCommand echo = new EchoCommand();
         Exec exec = new Exec();
         exec.execute(echo);
