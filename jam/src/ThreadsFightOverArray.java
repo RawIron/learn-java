fb13cb85a63d8e6021e3f31ece24631ffc18b724
RawIron <manianology@gmail.com>
2016-03-16 22:28:12 +0100

    add loop for reader; clean code

--- jam/src/ThreadsFightOverArray.java
+++ jam/src/ThreadsFightOverArray.java
@@ -7,43 +7,63 @@ class ArrayRandomInit {
 			job[i] = 0;
 		}		
 	}
+	
 	static public int fill(int[] job) {
 		Random g = new Random();
 		int totalEntries = g.nextInt(job.length);
 		for (int i=0; i<totalEntries; ++i) {
 			job[i] = 1;
 		}
+		System.out.println("should find " + totalEntries);
 		return totalEntries;
+	}
+
+	static public int count(int[] job) {
+		int total = 0;
+		for (int i=0; i<job.length; ++i) {
+			if (job[i] != 0) {
+				++total;
+			}
+		}
+		System.out.println("found " + total);
+		return total;
 	}
 }
 
 
 class ArrayWriter implements Runnable {
+	private static final int LOOPS = 50;
+	private static final int START_DELAY_MILLIS = 500;
+	private static final int ITERATE_DELAY_MILLIS = 2;
 	private int[] job;
 	private Random g;
-	
+
 	public ArrayWriter(int[] job) {
 		this.job = job;
 		this.g = new Random();
 	}
 	
 	protected void refill() {
-		for (int i=0; i<100; ++i) {
-			synchronized(job) {
-				ArrayRandomInit.erase(job);
-				ArrayRandomInit.fill(job);
-			}
-			try { Thread.sleep(g.nextInt(2)); } catch (Exception e) {}
+		synchronized(job) {
+			ArrayRandomInit.erase(job);
+			ArrayRandomInit.fill(job);		
 		}
+		try { Thread.sleep(g.nextInt(ITERATE_DELAY_MILLIS)); } catch (Exception e) {}
 	}
 	
 	public void run() {
-		refill();
+		try { Thread.sleep(g.nextInt(START_DELAY_MILLIS)); } catch (Exception e) {}
+		for (int i=0; i<LOOPS; ++i) {
+			refill();
+		}
 		return;
 	}
 }
 
 class ArrayReader implements Runnable {
+	private static final int LOOPS = 50;
+	private static final int START_DELAY_MILLIS = 500;
+	private static final int ITERATE_DELAY_MILLIS = 4;
 	private int[] job;
 	private Random g;
 	
@@ -51,37 +71,32 @@ class ArrayReader implements Runnable {
 		this.job = job;
 		this.g = new Random();
 	}
-	
+
 	protected int countJob() {
-		return countJob(10);
-	}
-	protected int countJob(int high) {
 		int total = 0;
 		synchronized(job) {
-			for (int i=0; i<job.length; ++i) {
-				if (job[i] != 0) {
-					++total;
-				}
-				try { Thread.sleep(g.nextInt(2)); } catch (Exception e) {}
-			}
+			total = ArrayRandomInit.count(job);
+			try { Thread.sleep(g.nextInt(ITERATE_DELAY_MILLIS)); } catch (Exception e) {}
 		}
 		return total;
 	}
 	
 	public void run() {
-		int found = countJob(100);
-		System.out.println("read:" + found);
+		try { Thread.sleep(g.nextInt(START_DELAY_MILLIS)); } catch (Exception e) {}		
+		for (int i=0; i<LOOPS; ++i) {
+			countJob();
+		}
 		return;
 	}
 }
 
 
 public class ThreadsFightOverArray {
+	private static final int ENTRIES = 100;
 
-	public static void main(String[] args) {
-		int[] test1 = new int[30];
-		int totalFilled = ArrayRandomInit.fill(test1);
-		System.out.println("array has " + totalFilled);
+	public static void main(String[] args) {	
+		int[] test1 = new int[ENTRIES];
+		ArrayRandomInit.fill(test1);
 		
 		System.out.println("start threads");		
 		Thread r = new Thread(new ArrayReader(test1));
