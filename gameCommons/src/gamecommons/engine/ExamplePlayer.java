64e4ed82036f1a42e341be1bff21a50107ac2bc7
RawIron <manianology@gmail.com>
2016-04-08 15:06:42 +0200

    add Valuable to examples

--- gameCommons/src/gamecommons/engine/ExamplePlayer.java
+++ gameCommons/src/gamecommons/engine/ExamplePlayer.java
@@ -3,20 +3,30 @@ package gamecommons.engine;
 
 class ExamplePlayer {
     class GameItem { }
-
     class Level { }
+    class Valuable {}
 
     class Player {
         void earned(GameItem item) { }
+        void earned(Valuable v) { }
+
         void completed(Level level) { }
+
+        Player give(GameItem g) { return this; }
+        Player take(Valuable v) { return this; }
     }
 
     Player player = new Player();
     GameItem premium = new GameItem();
+    GameItem building = new GameItem();
     Level level = new Level();
+    Valuable gold = new Valuable();
+
 
     public void example() {
         player.earned(premium);
         player.completed(level);
+        player.give(building).take(gold);
+        player.take(gold).give(building);
     }
 }
