66a3fab1e8b366ac9e177919cc12f29e1da08dce
RawIron <manianology@gmail.com>
2016-04-11 10:38:18 +0200

    fix compile error

--- gameCommons/src/gamecommons/system/jdbcDB.java
+++ gameCommons/src/gamecommons/system/jdbcDB.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
-import gamecommons.balanceSettings.DbManager;
-
 
 public class jdbcDB implements DataStore {
 
