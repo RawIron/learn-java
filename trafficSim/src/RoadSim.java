647cd12dd4e875c7839bb163a7ff394561e19cc5
RawIron <manianology@gmail.com>
2025-06-09 10:02:14 -0700

    Refactor to reduce complexity. Remove inheritance, remove shallow class.

--- trafficSim/src/RoadSim.java
+++ trafficSim/src/RoadSim.java
@@ -4,20 +4,23 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 
-public class RoadFrame extends Frame {
+public class RoadSim {
 
-    public RoadFrame() {
-        setSize(800, 200);
-        setVisible(true);
-        addWindowListener(new WindowAdapter() {
+    public RoadSim() {
+    }
+
+    public void init() {
+        Frame frame = new Frame();
+        frame.setSize(800, 200);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-    }
 
-     public void init() {
         slowdown = new Scrollbar(Scrollbar.HORIZONTAL, 0, 0, 0, 100);
         slowdown.setUnitIncrement(10);
         slowdown.setValue(5);
@@ -35,30 +38,30 @@ public class RoadFrame extends Frame {
         p.add(arrival);
         p.add(new Label(""));
 
-        canvas = new RoadCanvas();
+        canvas = new Canvas();
         canvas.setBackground(Color.black);
 
-        setLayout(new BorderLayout());
-        add("North", p);
-        add("Center", canvas);
+        frame.setLayout(new BorderLayout());
+        frame.add("North", p);
+        frame.add("Center", canvas);
 
         canvas.createBufferStrategy(2);
         buffer = canvas.getBufferStrategy();
     }
 
-    public double getSlowdown() {
-        return 0.01 * slowdown.getValue();
-    }
-
-    public double getArrival() {
-        return 0.01 * arrival.getValue();
-
-    }
-
     public void run() {
+        freeway = new Road();
         for ( ;; ) {
-            canvas.update(buffer.getDrawGraphics(), getSlowdown(), getArrival());
+            double probabilitySlowdown = 0.01 * slowdown.getValue();
+            double probabilityArrival = 0.01 * arrival.getValue();
+
+            Graphics g = buffer.getDrawGraphics();
+            freeway.clear(g, ROW, XDOTDIST, DOTSIZE);
+            freeway.update(probabilitySlowdown, probabilityArrival);
+            freeway.paint(g, ROW, XDOTDIST, DOTSIZE);
+            g.dispose();
             buffer.show();
+
             try {
                 Thread.sleep(300);
             } catch (InterruptedException e) {}
@@ -66,54 +69,32 @@ public class RoadFrame extends Frame {
      }
 
     public static void main(String[] args) {
-        RoadFrame f = new RoadFrame();
-        f.init();
-        f.run();
+        RoadSim sim = new RoadSim();
+        sim.init();
+        sim.run();
    }
 
-    private RoadCanvas canvas;
-    private BufferStrategy buffer;
     private Scrollbar slowdown;
     private Scrollbar arrival;
-    private Thread runner;
-}
-
-
-class RoadCanvas extends Canvas {
-    public RoadCanvas() {
-        freeway = new Road();
-        row = 29;
-    }
-
-    public void update(Graphics g, double slowdown, double arrival) {
-        //xsize = getWidth();
-        //ysize = getHeight;
-        freeway.clear(g, row, XDOTDIST, DOTSIZE);
-        freeway.update(slowdown, arrival);
-        freeway.paint(g, row, XDOTDIST, DOTSIZE);
-        g.dispose();
-    }
-
-    private final int DOTSIZE = 4;
-    private final int XDOTDIST = 5;
+    private Canvas canvas;
+    private BufferStrategy buffer;
+    private static final int DOTSIZE = 4;
+    private static final int XDOTDIST = 5;
+    private static final int ROW = 29;
     private Road freeway;
-    private int row;
-    private int xsize;
-    private int ysize;
 }
 
 
 class Road {
-    private static final int EMPTY = -1;
 
     public Road() {
         speed = new int[LENGTH];
         colors = new Color[LENGTH];
-        count = 0;
         for (int i = 0; i < LENGTH; i++) { speed[i] = EMPTY; }
+        count = 0;
     }
 
-    public void update(double prob_slowdown, double prob_create) {
+    public void update(double probabilitySlowdown, double probabilityArrival) {
         int i = 0;
 
         // skip location with no vehicle
@@ -122,7 +103,7 @@ class Road {
 
         while (i < LENGTH) {
             // randomly adjust speed of vehicle at current location
-            if (Math.random() <= prob_slowdown && speed[i] > 0)
+            if (Math.random() <= probabilitySlowdown && speed[i] > 0)
                 speed[i]--;
             else if (speed[i] < MAXSPEED)
                 speed[i]++;
@@ -157,7 +138,7 @@ class Road {
 
         // randomly decide whether a new vehicle arrives
         // new vehicle has random speed
-        if (Math.random() <= prob_create && speed[0] == EMPTY) {
+        if (Math.random() <= probabilityArrival && speed[0] == EMPTY) {
             speed[0] = (int) (5.99 * Math.random());
             colors[0] = ++count % 10 == 0 ? Color.red : Color.blue;
         }
@@ -181,8 +162,9 @@ class Road {
         }
     }
 
-    public static final int LENGTH = 160;
-    public static final int MAXSPEED = 5;
+    private static final int EMPTY = -1;
+    private static final int LENGTH = 160;
+    private static final int MAXSPEED = 5;
 
     private int[] speed;
     private Color[] colors;
